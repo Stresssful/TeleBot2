3062a11ad1f2ac74303f876d4e7916b22da9404f
Etherdrake <vivern07@gmail.com>
2017-11-19 17:54:07 +0200

    resending (every 15min)

--- bot.js
+++ bot.js
@@ -4,6 +4,13 @@ var TelegramBot = require('node-telegram-bot-api');
 var monk = require('monk');
 
 var db = monk('ether:herokuDB@ds249025.mlab.com:49025/heroku_26kgq0gk');
+var users = db.get('users'); //таблиця користувачів
+var update = db.get('last_update'); //останній апдейт
+
+
+var token = '418440998:AAGGlVniBHyN9F1t2ckmOGm6CWel37p1G_A';// Устанавливаем токен
+var bot = new TelegramBot(token, {polling: true});// Включить опрос сервера
+setInterval(intervalFunc, 900000);// Перевірка наявності оновлень (900000 - 15 хв, 3600000 - 1 год)
 
   function getReplacements(GROUP, callback)
   {
@@ -61,10 +68,91 @@ var db = monk('ether:herokuDB@ds249025.mlab.com:49025/heroku_26kgq0gk');
         });
   }
 
-    // Устанавливаем токен, который выдавал нам бот.
-    var token = '418440998:AAGGlVniBHyN9F1t2ckmOGm6CWel37p1G_A';
-    // Включить опрос сервера
-    var bot = new TelegramBot(token, {polling: true});
+    function getReplacementsFromLoaded(content,tabl,anoun,callback,GROUP) //Заміни з вже завантаженої сторінки
+    {
+          let date=content.eq(0).text(); //Дата
+          let day=content.eq(1).text(); //Чисельник\знаменник
+
+          let table=tabl //Заміни
+
+          let anouncementsRaw=anoun; //Всі оголошення
+          let anouncements;
+          if(anouncementsRaw.length>0)anouncements="Оголошення:\n";
+          else anouncements="Оголошень немає\n";
+
+          for(let i=0;i<anouncementsRaw.length; i++)
+          {
+            anouncements+="\t"+anouncementsRaw.eq(i).text()+"\n"; //Розбиття оголошень по рядках
+          }
+          let output=GROUP+":\n"+date+"\n"+day+"\n"+anouncements+"\n"+"Заміни:\n";
+
+
+          let prevGroup='';
+          let empty=true;
+          for(let i=1; i<table.length; i++)
+          {
+            let group=table.eq(i).children().eq(0).text(); //0-Група
+            let pair=table.eq(i).children().eq(1).text(); //1-Пара
+            //2-Кого замінили
+            let subject=table.eq(i).children().eq(3).text(); //3-Предмет
+            let teacher=table.eq(i).children().eq(4).text(); //4-Викладач
+            let room=table.eq(i).children().eq(5).text(); //5-Аудиторія
+
+            if(group.includes("-")) //Якщо клітинка з групою не порожня -- там буде -
+            {
+              prevGroup=group;
+              if(group==GROUP)
+              {
+                output+="\t"+pair+"\t"+subject+"\t\t"+teacher+"\t"+room+"\n";
+                empty=false;
+              }         
+            }
+            else if(!group.includes("-") && prevGroup==GROUP)         
+            {
+              output+="\t"+pair+"\t"+subject+"\t\t"+teacher+"\t"+room+"\n";
+            }         
+          }
+          
+        if(empty) output+="Замін немає";
+        callback(null, output);
+      }
+
+      function intervalFunc() //функція для таймера
+      { 
+        update.find({},function(err,last)
+        {
+            if (err) throw err;
+            let lastUpdate=last[0].LastDate;
+            request({uri:'http://hpk.edu.ua/replacements', method:'GET', encoding:'utf-8'},
+            function (err, res, page) 
+            {        
+                let $=cheerio.load(page); 
+                let content=$('div.news-body').children();          
+
+                let date=content.eq(0).text(); //Дата
+                if(date!=lastUpdate) //Якщо дата не така як в БД
+                {
+                  let tabl=$('div.news-body > table > tbody').children(); //Заміни
+                  let anoun=$('[colspan=6]'); //Всі оголошення
+                  update.remove({LastDate:lastUpdate}); //Записуєм дату в БД
+                  update.insert(
+                  {                
+                    LastDate: date
+                  });
+                  users.find({},function(err,doc) 
+                  {
+                      if (err) throw err;
+                      for(let i=0;i<doc.length;i++)
+                      {
+                        getReplacementsFromLoaded(content,tabl,anoun,function(err, msg){bot.sendMessage(doc[i].id, msg)},doc[i].Group)
+                      }
+                  });
+                }
+            });
+        });
+      }
+
+
 
 
     bot.onText(/^\D\D-\d\d\d$/, function(msg, match) { // \D - буква; \d - цифра
@@ -83,23 +171,26 @@ var db = monk('ether:herokuDB@ds249025.mlab.com:49025/heroku_26kgq0gk');
       bot.sendMessage(fromId, group, options);
     });
 
-    bot.onText(/\/my/, function(msg, match) { 
+    bot.onText(/\/my/, function(msg, match) { //команда \my
       let fromId = msg.from.id;
-      let collection = db.get('users');
-      collection.findOne({ id: fromId }, function(err, doc)
+      users.findOne({ id: fromId }, function(err, doc)
       {
           if (err) throw err;
-          let group=doc.Group;
-          getReplacements(group, function(err, msg){bot.sendMessage(fromId, msg)});
+          if(doc==null)bot.sendMessage(fromId,"Ви не відслідковуєте жодну групу!");
+          else
+          {
+            let group=doc.Group;
+            getReplacements(group, function(err, msg){bot.sendMessage(fromId, msg)});
+          }
       });
     });
 
-    bot.onText(/\/remove/, function(msg, match) { 
+    bot.onText(/\/remove/, function(msg, match) { //команда \remove
       let fromId = msg.from.id;
-      unsubscribe(fromId);      
+      users.remove({ id:fromId});      
     });
 
-    bot.on('callback_query', function (msg) {
+    bot.on('callback_query', function (msg) { //обробка кнопок відслідкувати або переглянути заміну
       let query = msg.data.split(':');
       let group = query[0];
       let action = query[1];
@@ -115,21 +206,14 @@ var db = monk('ether:herokuDB@ds249025.mlab.com:49025/heroku_26kgq0gk');
       }
     });
 
-    function addToBase(telegramID, group, name)
+    function addToBase(telegramID, group, name) //Запис користувачів в БД
     {
-      let collection = db.get('users');
-      //collection.remove({ id: telegramID });
+      users.remove({ id: telegramID });
 
-      collection.insert(
+      users.insert(
       { 
         id: telegramID, 
         Group: group,
         Name: name
       });
-    }
-
-    function unsubscribe(telegramID)
-    {
-      let collection = db.get('users');
-      collection.remove({ id: telegramID });
     }
--- parsing.js
+++ parsing.js
@@ -64,16 +64,184 @@ request({uri:'http://hpk.edu.ua/replacements', method:'GET', encoding:'utf-8'},
 });    
 */
 
+function getReplacements(GROUP, callback)
+  {
+    
+      request({uri:'http://hpk.edu.ua/replacements', method:'GET', encoding:'utf-8'},
+      function (err, res, page) {        
+          let $=cheerio.load(page); 
+          let content=$('div.news-body').children();
+
+          let date=content.eq(0).text(); //Дата
+          let day=content.eq(1).text(); //Чисельник\знаменник
+
+          let table=$('div.news-body > table > tbody').children(); //Заміни
+
+          let anouncementsRaw=$('[colspan=6]'); //Всі оголошення
+          let anouncements;
+          if(anouncementsRaw.length>0)anouncements="Оголошення:\n";
+          else anouncements="Оголошень немає\n";
+
+          for(let i=0;i<anouncementsRaw.length; i++)
+          {
+            anouncements+="\t"+anouncementsRaw.eq(i).text()+"\n"; //Розбиття оголошень по рядках
+          }
+          let output=GROUP+":\n"+date+"\n"+day+"\n"+anouncements+"\n"+"Заміни:\n";
+
+
+          let prevGroup='';
+          let empty=true;
+          for(let i=1; i<table.length; i++)
+          {
+            let group=table.eq(i).children().eq(0).text(); //0-Група
+            let pair=table.eq(i).children().eq(1).text(); //1-Пара
+            //2-Кого замінили
+            let subject=table.eq(i).children().eq(3).text(); //3-Предмет
+            let teacher=table.eq(i).children().eq(4).text(); //4-Викладач
+            let room=table.eq(i).children().eq(5).text(); //5-Аудиторія
+
+            if(group.includes("-")) //Якщо клітинка з групою не порожня -- там буде -
+            {
+              prevGroup=group;
+              if(group==GROUP)
+              {
+                output+="\t"+pair+"\t"+subject+"\t\t"+teacher+"\t"+room+"\n";
+                empty=false;
+              }         
+            }
+            else if(!group.includes("-") && prevGroup==GROUP)         
+            {
+              output+="\t"+pair+"\t"+subject+"\t\t"+teacher+"\t"+room+"\n";
+            }         
+          }
+          
+          if(empty) output+="Замін немає";
+          callback(null, output);
+        });
+  }
+
+  function getReplacementsFromLoaded(content,tabl,anoun,callback,GROUP)
+  {
+          let date=content.eq(0).text(); //Дата
+          let day=content.eq(1).text(); //Чисельник\знаменник
+
+          let table=tabl //Заміни
+
+          let anouncementsRaw=anoun; //Всі оголошення
+          let anouncements;
+          if(anouncementsRaw.length>0)anouncements="Оголошення:\n";
+          else anouncements="Оголошень немає\n";
+
+          for(let i=0;i<anouncementsRaw.length; i++)
+          {
+            anouncements+="\t"+anouncementsRaw.eq(i).text()+"\n"; //Розбиття оголошень по рядках
+          }
+          let output=GROUP+":\n"+date+"\n"+day+"\n"+anouncements+"\n"+"Заміни:\n";
+
+
+          let prevGroup='';
+          let empty=true;
+          for(let i=1; i<table.length; i++)
+          {
+            let group=table.eq(i).children().eq(0).text(); //0-Група
+            let pair=table.eq(i).children().eq(1).text(); //1-Пара
+            //2-Кого замінили
+            let subject=table.eq(i).children().eq(3).text(); //3-Предмет
+            let teacher=table.eq(i).children().eq(4).text(); //4-Викладач
+            let room=table.eq(i).children().eq(5).text(); //5-Аудиторія
+
+            if(group.includes("-")) //Якщо клітинка з групою не порожня -- там буде -
+            {
+              prevGroup=group;
+              if(group==GROUP)
+              {
+                output+="\t"+pair+"\t"+subject+"\t\t"+teacher+"\t"+room+"\n";
+                empty=false;
+              }         
+            }
+            else if(!group.includes("-") && prevGroup==GROUP)         
+            {
+              output+="\t"+pair+"\t"+subject+"\t\t"+teacher+"\t"+room+"\n";
+            }         
+          }
+          
+        if(empty) output+="Замін немає";
+        //callback(null, output);
+        callback(output);
+  }
+
+  /*function getDate()
+  {
+    request({uri:'http://hpk.edu.ua/replacements', method:'GET', encoding:'utf-8'},
+      function (err, res, page) 
+      {        
+          let $=cheerio.load(page); 
+          let content=$('div.news-body').children();
+
+          let date=content.eq(0).text(); //Дата
+          let day=content.eq(1).text();
+      }
+  }*/
+
 var monk = require('monk');
+var request = require('request');
+var cheerio = require('cheerio');
 var db = monk('ether:herokuDB@ds249025.mlab.com:49025/heroku_26kgq0gk');
-var collection = db.get('users');
+var users = db.get('users');
+var update = db.get('last_update');
 var ID=310694905;
 
-collection.findOne({ id: ID }, function(err, doc)
+
+function intervalFunc() 
+{ 
+  update.find({},function(err,last)
+  {
+      if (err) throw err;
+      let lastUpdate=last[0].LastDate;
+      request({uri:'http://hpk.edu.ua/replacements', method:'GET', encoding:'utf-8'},
+      function (err, res, page) 
+      {        
+          let $=cheerio.load(page); 
+          let content=$('div.news-body').children();          
+
+          let date=content.eq(0).text(); //Дата
+          if(date!=lastUpdate)
+          {
+            let tabl=$('div.news-body > table > tbody').children(); //Заміни
+            let anoun=$('[colspan=6]'); //Всі оголошення
+            update.remove({LastDate:lastUpdate});
+            update.insert(
+            {                
+              LastDate: date
+            });
+            users.find({},function(err,doc)
+            {
+                if (err) throw err;
+                for(let i=0;i<doc.length;i++)
+                {
+                  //console.log(doc[i].Group);
+                  getReplacementsFromLoaded(content,tabl,anoun,console.log,doc[i].Group)
+                }
+            });
+          }
+      });
+      //getReplacements(group, function(err, msg){console.log(msg)});
+      //console.log(group);
+  });
+}
+
+    /*users.findOne({ id: 310694905 }, function(err, doc)
     {
-        if (err) throw err;
-        console.log(doc.Name);
-    });
+          if (err) throw err;
+          let group=doc.Group;
+          getReplacements(group, function(err, msg){console.log(msg)});
+     });*/
+
+setInterval(intervalFunc, 1500);
+
+//3600000=1hour
+
+
 
 /*
 	0 - Група
